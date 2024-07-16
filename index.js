d23312b835104e1b47a33b52fb7f7e8704beaded
mashigovincent01 <mashigovincent407@gmail.com>
2024-07-16 09:05:54 +0200

    add a function for creating cards and for printing books

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    const myLibrary = [];
+    const myLibrary = [
+        new Book("Title", "J.K Rolling", 200, true),
+        new Book("The punisher", "G Rabothata", 100, false),
+        new Book("Drama", "OK Hotane", 1, false)
+    ];
 
     function Book(title, author, pages, read){
         //constructor
@@ -13,11 +17,29 @@ document.addEventListener("DOMContentLoaded", ()=>{
         }
     }
     
+    function createCard(book){
+        return `
+        <div class="card">
+            <h2>${book.title}</h2>
+            <h3>${book.author}</h3>
+            <h3>${book.pages} ${book.pages > 1 ? "pages" : "page"}</h3>
+            <h3>Read: ${book.read ? "Yes" : "No"}</h3>
+        </div>
+        `
+    }
+    function printBooks(){
+        const main = document.querySelector(".main");
+        main.innerHTML = "";
+
+        myLibrary.forEach((book)=>{
+            main.innerHTML += createCard(book);
+        });
+    }
     function addBookToLibrary(){
         //do stuff here
     
     }
     
-    const myBook = new Book("The Hobbit", "J.R.R.", 1, false);
+    printBooks();
     
 });
